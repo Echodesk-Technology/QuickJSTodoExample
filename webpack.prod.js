f2dd25aa623ba7df048031dc8bebd17b477132c9
Serverguyken <serverguyken@gmail.com>
2021-03-15 15:32:11 -0400

    changed webpack production path

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,7 +9,7 @@ module.exports = {
   name: "prod",
   output: {
     filename: 'app.js',
-    path: path.resolve(__dirname, 'dist/'),
+    path: path.resolve(__dirname, 'public/'),
   },
   entry: '/public/index.js',
   target: 'node',
