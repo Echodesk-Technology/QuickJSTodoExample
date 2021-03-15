7ae79899f88dbebae2230baa3ff7bb362c66d0c4
Serverguyken <serverguyken@gmail.com>
2021-03-15 15:34:37 -0400

    changed build script

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,7 +9,7 @@ module.exports = {
   name: "prod",
   output: {
     filename: 'app.js',
-    path: path.resolve(__dirname, 'public/'),
+    path: path.resolve(__dirname, 'public'),
   },
   entry: '/public/index.js',
   target: 'node',
