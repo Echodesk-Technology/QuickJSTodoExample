71cfa84365938057d466fd1679dddd9eb768dfa8
Serverguyken <serverguyken@gmail.com>
2021-03-15 20:50:07 -0400

    removed livereload

--- public/index.js
+++ public/index.js
@@ -1,10 +1,2 @@
 import Quick from "quickjs-component";
 import routes from "../src/router/routes";
-import config from "../quick.config";
-
-if(config.mode === "production") {
-   setTimeout(() => {
-    document.getElementsByTagName("script")[1].remove();
-    console.error = function(){}
-   },0)
-}
